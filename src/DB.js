1ad62320de80560149600d402f8fbb40052e2bf6
elv-zhounan <zhounan@eluv.io>
2023-08-09 17:51:56 +0800

    seperate the DB util functions

---
+++ src/DB.js
@@ -0,0 +1,99 @@
+import firebaseConfig from "./configuration";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  Timestamp,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
+
+class DB {
+  constructor(firebaseConfig) {
+    const app = initializeApp(firebaseConfig);
+    this.db = getFirestore(app);
+  }
+
+  async saveUser({ clientAddr }) {
+    if (this.db !== null) {
+      try {
+        const userDocRef = doc(this.db, "User", clientAddr);
+        const userDocSnap = await getDoc(userDocRef);
+        if (userDocSnap.exists()) {
+          console.log("User already exists");
+        } else {
+          await setDoc(clientRef, {
+            Client_address: clientAddr,
+            Wallet_id: null,
+            Email_add: null,
+            Creation_time: null,
+            Updated_time: null,
+            Personal_info: {},
+          });
+        }
+      } catch (err) {
+        console.log("Save user into DB err");
+      }
+    }
+  }
+
+  async saveEngagement({ searchId, clientAddr, engagement, init = false }) {
+    if (this.db !== null) {
+      try {
+        const engDocRef = doc(
+          this.db,
+          "Engagement",
+          clientAddr + "_" + searchId
+        );
+        if (init) {
+          await setDoc(engDocRef, {
+            engagement: engagement,
+            User_id: clientAddr,
+            Search_id: searchId,
+          });
+        } else {
+          await updateDoc(engDocRef, {
+            engagement: engagement,
+          });
+        }
+
+        console.log(`Engagement table saved`);
+      } catch (err) {
+        console.log("Error occured when initializing the engagement table");
+        console.log(err);
+      }
+    }
+  }
+
+  async saveSearchHistory({
+    clientAddr,
+    fuzzysearchPhrase,
+    fuzzySearchFields,
+    searchKeywords,
+  }) {
+    if (db !== null) {
+      try {
+        const colRef = collection(this.db, "Search_history");
+        const now = Timestamp.now().toDate().toUTCString();
+        const docRef = await addDoc(colRef, {
+          client: clientAddr,
+          search_time: now,
+          fuzzySearchPhrase: fuzzysearchPhrase,
+          fuzzySearchFields: fuzzySearchFields,
+          searchKeywords: searchKeywords,
+        });
+        console.log(`search history updated with docID ${docRef.id}`);
+        return docRef.id;
+      } catch (err) {
+        console.log("Error occured when storing the search history");
+        console.log(err);
+        return null;
+      }
+    }
+  }
+}
+
+export default DB;
