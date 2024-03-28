014d773c72d3729a59862c140cbd4051d1e28d4f
elv-zhounan <zhounan@eluv.io>
2024-03-28 12:25:06 -0700

    Show time for summary

--- src/components/SummaryPad.js
+++ src/components/SummaryPad.js
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { BsCaretLeftFill, BsCaretRightFill } from "react-icons/bs";
-
+import { toTimeString } from "../utils";
 
 const SummaryPad = (props) => {
   const [summaries, setSummaries] = useState([])
   const [displaying, setDisplaying] = useState(0)
+  const [summaryTimestamps, setSummaryTimestamps] = useState([])
+  
   useEffect(() => {
     try{
-      const _summaries = getSummary();
-      setSummaries(_summaries)
-      console.log(_summaries)
+      console.log("paring summarizations")
+      const [_summaries, _times] = getSummaryWithTime();
+      if (_summaries.length > 0){
+        setSummaries(_summaries)
+        setSummaryTimestamps(_times)
+      } else {
+        const _summaries_backup = getSummary();
+        setSummaries(_summaries_backup)
+      }
     } catch(err) {
       setSummaries([])
       console.log("Parse llava caption failed")
@@ -19,13 +27,50 @@ const SummaryPad = (props) => {
   const getSummary = () => {
     const res = []
     for(let source of props.clipInfo.sources){
-      if("f_lava" in source["fields"]){
-        for(let summary of source["fields"]["f_lava"]){
+      if("f_llava" in source["fields"]){
+        for(let summary of source["fields"]["f_llava"]){
           res.push(summary)
         }
       }
     }
     return res
+  }
+  const getSummaryWithTime = () => {
+    const summaries = []
+    const times = []
+
+    const summaries_with_time = []
+
+    try{
+      for(let source of props.clipInfo.sources){
+        if("f_llava_tag" in source["fields"]){
+          for(let summary of source["fields"]["f_llava_tag"]){
+            summaries_with_time.push({
+              start_time: summary["start_time"], 
+              end_time: summary["end_time"],
+              text: summary["text"][0]
+            })
+          }
+        }
+      }
+    } catch (err) {
+      console.log("Get summaries with time info failed, possible no time information")
+      return [[], []]
+    }
+
+    summaries_with_time.sort((a,b) => a.start_time - b.start_time);
+
+    for(let obj of summaries_with_time){
+      summaries.push(obj["text"])
+      times.push(
+        {
+          start_time: obj["start_time"], 
+          end_time: obj["end_time"]
+        }
+      )
+    }
+
+    return [summaries, times]
   }
 
   return (
@@ -46,7 +91,8 @@ const SummaryPad = (props) => {
           marginBottom: "5%",
           height: "15%",
           width: "90%",
-          border: "solid"
+          backgroundColor: "lightgray",
+          borderRadius: 10,
         }}
       >
         <div
@@ -56,7 +102,7 @@ const SummaryPad = (props) => {
             justifyContent: "center",
             alignItems: "center",
             height: "30%",
-            width: "15%",
+            width: "10%",
             cursor: "pointer"
           }}
           onClick={() => {
@@ -68,16 +114,59 @@ const SummaryPad = (props) => {
         <div
           style={{
             display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
+            flexDirection: "row",
+            justifyContent: "space-between",
             alignItems: "center",
             height: "90%",
-            width: "65%",
-            fontSize: 50,
+            width: "70%",
           }}
         >
-          {displaying}
+          {/* local tag idx */}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "90%",
+              width: "30%",
+            }}
+          >
+            <div 
+              style={{fontSize: 12}}  
+            >
+              # 
+            </div>
+            <div 
+              style={{fontSize: 20}}  
+            >
+              {summaryTimestamps.length > 0 ? displaying : ""}
+            </div>
+          </div>
+          {/* global timestamps */}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "90%",
+              width: "30%",
+            }}
+          >
+            <div 
+              style={{fontSize: 12}}  
+            >
+              Timestamp
+            </div>
+            <div 
+              style={{fontSize: 20}}  
+            >
+              {summaryTimestamps.length > 0 ? toTimeString(summaryTimestamps[displaying]["start_time"]) : ""}
+            </div>
+          </div>
         </div>
+        
         <div
           style={{
             display: "flex",
@@ -85,7 +174,7 @@ const SummaryPad = (props) => {
             justifyContent: "center",
             alignItems: "center",
             height: "30%",
-            width: "15%",
+            width: "10%",
             cursor: "pointer"
           }}
           onClick={() => {
@@ -108,7 +197,7 @@ const SummaryPad = (props) => {
           padding: 15,
         }}
       >
-        {summaries[displaying]}
+        {summaries.length > 0 ? summaries[displaying] : ""}
       </div>
     </div>
   )
