9a324696eb519e078bf9d4ea84b929a10519b711
elv-zhounan <zhounan@eluv.io>
2024-03-27 17:09:28 -0700

    Add controller to switch between showing tags and summary

--- src/components/QAPad.js
+++ src/components/QAPad.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import TagsPad from "./TagsPad";
+import { BsXLg } from "react-icons/bs";
 
 const container = {
   width: "30%",
@@ -31,7 +32,7 @@ const tagsContainer = {
   justifyContent: "flex-start",
   alignItems: "center",
   width: "100%",
-  height: 800,
+  height: 750,
   marginBottom: 20,
   backgroundColor: "transparent",
 };
@@ -39,7 +40,7 @@ const tagsContainer = {
 const ctrContainer = {
   display: "flex",
   flexDirection: "column",
-  justifyContent: "center",
+  justifyContent: "flex-start",
   alignItems: "center",
   width: "8%",
   height: "100%",
@@ -47,9 +48,49 @@ const ctrContainer = {
 
 const QAPad = (props) => {
   const [hidden, setHidden] = useState(true);
+  const [showTags, setShowTags] = useState(true)
   return !hidden ? (
     <div style={container}>
       <div style={QAContainer}>
+        <div
+          style={{
+            width: "90%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            marginBottom: 20,
+          }}
+        >
+          <div
+            onClick={() => {setShowTags(true)}}
+            style={{
+              cursor: "pointer",
+              paddingLeft: 10,
+              paddingRight: 10,
+              marginRight: 10,
+              marginLeft: 10,
+              borderRadius: 5,
+              backgroundColor: showTags ? "lightgray" : "transparent"
+            }}
+          >
+            Tags
+          </div>
+          <div
+            onClick={() => {setShowTags(false)}}
+            style={{
+              cursor: "pointer",
+              paddingLeft: 10,
+              paddingRight: 10,
+              marginRight: 10,
+              marginLeft: 10,
+              borderRadius: 5,
+              backgroundColor: showTags ? "transparent" : "lightgray"
+            }}
+          >
+            Summary
+          </div>
+        </div>
         <div style={tagsContainer}>
           <TagsPad
             clipInfo={props.clipInfo}
@@ -66,21 +107,18 @@ const QAPad = (props) => {
           style={{
             border: "none",
             width: "100%",
-            height: "10%",
-            backgroundColor: "lightgray",
-            borderTopLeftRadius: 10,
-            borderBottomLeftRadius: 10,
+            backgroundColor: "transparent",
           }}
           onClick={() => {
             setHidden(true);
           }}
         >
-          ◀️
+          <BsXLg />
         </button>
       </div>
     </div>
   ) : (
-    <div style={{ ...ctrContainer, width: "2%" }}>
+    <div style={{ ...ctrContainer, width: "2%", justifyContent: "center"}}>
       <button
         style={{
           border: "none",
@@ -93,6 +131,7 @@ const QAPad = (props) => {
         onClick={() => {
           setHidden(false);
         }}
+
       >
         ▶️
       </button>
